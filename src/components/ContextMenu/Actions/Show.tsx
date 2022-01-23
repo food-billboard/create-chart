c906b88b0d50996dac39f07374c7b29f8cf2c6b6
food-billboard <18368003190@163.com>
2022-01-23 14:42:29 +0800

    feat(): contextmenu show action

--- src/components/ContextMenu/Actions/Show.tsx
+++ src/components/ContextMenu/Actions/Show.tsx
@@ -1,13 +1,84 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
+import { useIdPathMap } from '@/hooks';
+import { CommonActionType } from './type';
 
-const ShowAction = () => {
-  const handleClick = useCallback(() => {
-    console.log('撤销');
-  }, []);
+const ShowAction = (props: CommonActionType) => {
+  const {
+    value,
+    path,
+    setComponent,
+    setComponentAll,
+    components,
+    select,
+    onClick,
+  } = props;
+  const {
+    id,
+    config: {
+      attr: { visible },
+    },
+  } = value;
+
+  const isShow = useMemo(() => {
+    // * 选中多个的情况下，统一显示隐藏
+    return visible && select.length === 1;
+  }, [visible, select]);
+
+  const title = useMemo(() => {
+    return isShow ? '隐藏' : '显示';
+  }, [isShow]);
+
+  const handleClick = useCallback(
+    (e: any) => {
+      e.stopPropagation();
+      if (isShow) {
+        setComponent({
+          value: {
+            config: {
+              attr: {
+                visible: false,
+              },
+            },
+          },
+          id,
+          path,
+          action: 'update',
+        });
+      } else {
+        const idPathMap = useIdPathMap();
+
+        const updateComponent = select.reduce<
+          ComponentMethod.SetComponentMethodParamsData[]
+        >((acc, cur) => {
+          const target = idPathMap[cur];
+          if (target) {
+            const { path, id } = target;
+            acc.push({
+              path,
+              id,
+              action: 'update',
+              value: {
+                config: {
+                  attr: {
+                    visible: true,
+                  },
+                },
+              },
+            });
+          }
+          return acc;
+        }, []);
+
+        setComponent(updateComponent);
+      }
+      onClick();
+    },
+    [isShow, id, path, components, setComponentAll, select, onClick],
+  );
 
   return (
     <div key="show" onClick={handleClick}>
-      置底
+      {title}
     </div>
   );
 };
