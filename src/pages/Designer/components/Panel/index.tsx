951eea10806f1fdd20c94d4dc3a5f03d82dc700c
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-02-14 14:10:04 +0800

    fix(): :bug: panel style

--- src/pages/Designer/components/Panel/index.tsx
+++ src/pages/Designer/components/Panel/index.tsx
@@ -1,11 +1,15 @@
+import classnames from 'classnames';
 import ReactSelecto from './components/ReactSelecto';
 import PanelWrapper from './components/PanelWrapper';
 import ToolBar from './components/ToolBar';
 import Painter from './components/Painter';
+import styles from './index.less';
 
 const Panel = () => {
   return (
-    <div className="dis-flex-column">
+    <div
+      className={classnames(styles['design-panel-content'], 'dis-flex-column')}
+    >
       <PanelWrapper>
         <ReactSelecto />
         <Painter />
