3404ae44258c26b0cf99e46e35153df3e270c0d6
food-billboard <18368003190@163.com>
2022-03-24 21:59:46 +0800

    fix(): 跳转路径修改

--- src/utils/tool.ts
+++ src/utils/tool.ts
@@ -80,21 +80,21 @@ export function mergeWithoutArray(...args: any[]) {
 // 预览
 export function goPreview(id: string) {
   const { origin, pathname } = new URL(location.href);
-  const url = origin + pathname + '/#/preview?id=' + id;
+  const url = origin + pathname + '#/preview?id=' + id;
   window.open(url, '_blank');
 }
 
 // 分享
 export function goShare(id: string) {
   const { origin, pathname } = new URL(location.href);
-  const url = origin + pathname + '/#/share?id=' + id;
+  const url = origin + pathname + '#/share?id=' + id;
   window.open(url, '_blank');
 }
 
 // 设计页面
 export function goDesign(id?: string) {
   const { origin, pathname } = new URL(location.href);
-  const baseUrl = origin + pathname + '/#/designer';
+  const baseUrl = origin + pathname + '#/designer';
   const url = id ? baseUrl + '?id=' + id : baseUrl;
   window.open(url, '_blank');
 }
