d047c43c44ae0180d23195fa5120cea88e63bdd4
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-06-10 14:41:23 +0800

    fix(): :bug: 组内组无返回按钮

--- src/pages/Designer/components/RightContent/components/GroupConfig/index.tsx
+++ src/pages/Designer/components/RightContent/components/GroupConfig/index.tsx
@@ -1,19 +1,22 @@
-import { useMemo } from 'react';
+import { useMemo, useCallback } from 'react';
 import classnames from 'classnames';
+import { connect } from 'dva';
 import { getComponent, isComponentDisabled } from '@/utils/Assist/Component';
 import ConfigWrapper, {
   ConfigItem,
 } from '@/components/ChartComponents/Common/ConfigWrapper';
 import BaseConfig from '@/components/ChartComponents/Common/BaseConfig';
 import OptionConfig from './components/OptionConfig';
+import { mapStateToProps, mapDispatchToProps } from './connect';
 import styles from './index.less';
 
 const GroupConfig = (props: {
   id: string;
   components: ComponentData.TComponentData[];
   component: ComponentData.TComponentData;
+  setSelect: (value: string[]) => void;
 }) => {
-  const { id, components, component } = props;
+  const { id, components, component, setSelect } = props;
 
   const title = useMemo(() => {
     const component: ComponentData.TComponentData = getComponent(
@@ -25,6 +28,15 @@ const GroupConfig = (props: {
 
   const disabled = isComponentDisabled(id);
 
+  const onBack = useCallback(() => {
+    const { parent } = component;
+    setSelect([parent!]);
+  }, [component, setSelect]);
+
+  const hasBack = useMemo(() => {
+    return !!component?.parent;
+  }, [component]);
+
   return (
     <div
       className={classnames('h-100', styles['design-config-group'])}
@@ -32,7 +44,12 @@ const GroupConfig = (props: {
         pointerEvents: disabled ? 'none' : 'unset',
       }}
     >
-      <ConfigWrapper tabCounter={1} title={title}>
+      <ConfigWrapper
+        tabCounter={1}
+        title={title}
+        hasBack={hasBack}
+        onBack={onBack}
+      >
         <ConfigItem tab="组合配置" key="1">
           <BaseConfig isGroupComponent id={id} />
           <OptionConfig component={component} />
@@ -42,4 +59,4 @@ const GroupConfig = (props: {
   );
 };
 
-export default GroupConfig;
+export default connect(mapStateToProps, mapDispatchToProps)(GroupConfig);
