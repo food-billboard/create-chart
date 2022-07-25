04d2579bb4b460ffe222106c4ba59f80c92aefbd
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-07-25 15:08:11 +0800

    style(): :lipstick: loading 更改

--- src/components/PageLoading/index.tsx
+++ src/components/PageLoading/index.tsx
@@ -1,5 +1,23 @@
 import { PageLoading } from '@ant-design/pro-layout';
+import PacmanLoader from 'react-spinners/PacmanLoader';
+import ThemeUtil from '@/utils/Assist/Theme';
+import ColorSelect from '../ColorSelect';
+import styles from './index.less';
+
+const { getRgbaString } = ColorSelect;
 
 // loading components from code split
 // https://umijs.org/plugin/umi-plugin-react.html#dynamicimport
-export default PageLoading;
+// export default PageLoading;
+const Loading = () => {
+  return (
+    <PacmanLoader
+      className={styles['page-custom-loading']}
+      size={25}
+      loading
+      color={getRgbaString(ThemeUtil.generateNextColor4CurrentTheme(0))}
+    />
+  );
+};
+
+export default Loading;
