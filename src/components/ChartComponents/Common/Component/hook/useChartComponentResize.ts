4864d64008d82d47bdcc00c107470dc5286ad096
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-10-14 13:50:29 +0800

    fix(): :bug: 组件动画不连贯

--- src/components/ChartComponents/Common/Component/hook/useChartComponentResize.ts
+++ src/components/ChartComponents/Common/Component/hook/useChartComponentResize.ts
@@ -1,16 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import echarts from 'echarts';
 import { useDebounceFn, useSize, useUpdateEffect } from 'ahooks';
 import EventEmitter from '../EventEmitter';
 
 export function useChartComponentResize(instance: echarts.ECharts) {
   const size = useSize(instance?.getDom());
+  const domSizeChanged = useRef(false);
 
   const { run } = useDebounceFn(() => {
     instance && instance.resize();
   });
 
   useUpdateEffect(() => {
+    if (!domSizeChanged.current) {
+      domSizeChanged.current = true;
+      return;
+    }
     run();
   }, [size]);
 
