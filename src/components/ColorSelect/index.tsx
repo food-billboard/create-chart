504e4da3fd1cb1cfeb248da6826877b56c6b3824
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-10-22 14:27:04 +0800

    style(): :necktie: 颜色选择添加主题色到常用颜色

--- src/components/ColorSelect/index.tsx
+++ src/components/ColorSelect/index.tsx
@@ -9,10 +9,36 @@ import {
 import color from 'color';
 import classnames from 'classnames';
 import { merge, debounce, isEqual } from 'lodash';
-import Tooltip from '../ChartComponents/Common/Tooltip';
 import { DEFAULT_COLOR } from '@/utils/constants';
-import { getRgbaString, getHexString, getOpacity } from '@/utils/Assist/Theme';
+import ThemeUtil, {
+  getRgbaString,
+  getHexString,
+  getOpacity,
+} from '@/utils/Assist/Theme';
+import {
+  GLOBAL_EVENT_EMITTER,
+  EVENT_NAME_MAP,
+} from '@/utils/Assist/EventEmitter';
+import Tooltip from '../ChartComponents/Common/Tooltip';
 import styles from './index.less';
+
+const DEFAULT_PRESET_COLOR_LIST = [
+  '#D0021B',
+  '#F5A623',
+  '#F8E71C',
+  '#8B572A',
+  '#7ED321',
+  '#417505',
+  '#BD10E0',
+  '#9013FE',
+  '#4A90E2',
+  '#50E3C2',
+  '#B8E986',
+  '#000000',
+  '#4A4A4A',
+  '#9B9B9B',
+  '#FFFFFF',
+];
 
 // * 颜色选择组件
 
@@ -30,6 +56,13 @@ const ColorSelect = (props: TColorSelectProps) => {
   const [stateValue, setStateValue] = useState<ComponentData.TColorConfig>(
     value || DEFAULT_COLOR,
   );
+  const [presetColorList, setPresetColorList] = useState<string[]>(() => {
+    const colorList = ThemeUtil.currentThemeColor;
+    return [
+      ...colorList,
+      ...DEFAULT_PRESET_COLOR_LIST.filter((item) => !colorList.includes(item)),
+    ];
+  });
 
   const onInternalChange: ColorChangeHandler = useCallback((value) => {
     const rgb = value.rgb;
@@ -43,9 +76,32 @@ const ColorSelect = (props: TColorSelectProps) => {
     [stateValue],
   );
 
+  const onThemeChange = () => {
+    const colorList = ThemeUtil.currentThemeColor;
+    setPresetColorList((prev) => {
+      return [
+        ...colorList,
+        ...prev.filter((item) => !colorList.includes(item)),
+      ];
+    });
+  };
+
   useUpdateEffect(() => {
     value && setStateValue(value);
   }, [value]);
+
+  useEffect(() => {
+    GLOBAL_EVENT_EMITTER.addListener(
+      EVENT_NAME_MAP.SCREEN_THEME_CHANGE,
+      onThemeChange,
+    );
+    return () => {
+      GLOBAL_EVENT_EMITTER.removeListener(
+        EVENT_NAME_MAP.SCREEN_THEME_CHANGE,
+        onThemeChange,
+      );
+    };
+  }, []);
 
   useUnmount(() => {
     if (!isEqual(value, stateValue)) onChange?.(stateValue);
@@ -58,6 +114,7 @@ const ColorSelect = (props: TColorSelectProps) => {
           {...nextProps}
           onChange={debounce(onInternalChange, 20)}
           color={stateValue}
+          presetColors={presetColorList}
         />
       }
       trigger="click"
