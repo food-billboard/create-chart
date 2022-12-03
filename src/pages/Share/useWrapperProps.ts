b13d0598ca2c091605dd532f4a4fdf0ec48f885a
food-billboard <50391608+food-billboard@users.noreply.github.com>
2022-12-03 10:12:14 +0800

    fix(): :bug: pc预览显示错误

--- src/pages/Share/useWrapperProps.ts
+++ src/pages/Share/useWrapperProps.ts
@@ -16,7 +16,7 @@ const userWrapperProps = (
       'page-preview-container',
     ),
     style: {
-      transform: `scale(${scale}) translateX(${flag === 'PC' ? '50%' : '0'})`,
+      transform: `scale(${scale}) translateX(${flag === 'PC' ? '-50%' : '0'})`,
       minHeight: `calc( 100vh / ${scale} )`,
     },
     scale,
