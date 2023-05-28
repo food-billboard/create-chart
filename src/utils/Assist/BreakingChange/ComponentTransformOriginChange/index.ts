62c4952ee94f0372809698903747c7c62299a62c
food-billboard <18368003190@163.com>
2023-05-28 10:30:00 +0800

    fix(): :bug: version parse error

--- src/utils/Assist/BreakingChange/ComponentTransformOriginChange/index.ts
+++ src/utils/Assist/BreakingChange/ComponentTransformOriginChange/index.ts
@@ -1,7 +1,7 @@
 import { versionCompare } from '@/utils';
 
 export const ComponentTransformOriginChange = (version: string) => {
-  return !version || versionCompare(parseFloat(version), '1.6')
+  return !version || versionCompare(version, '1.6')
     ? 'center center'
     : 'left top';
 };
