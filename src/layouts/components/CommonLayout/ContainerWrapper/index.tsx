e32ec99ee2f5b237f71bbafec6243cd8f8387a77
food-billboard <50391608+food-billboard@users.noreply.github.com>
2024-02-23 10:39:30 +0800

    fix(): :bug: globalConfig初始值赋值失败

--- src/layouts/components/CommonLayout/ContainerWrapper/index.tsx
+++ src/layouts/components/CommonLayout/ContainerWrapper/index.tsx
@@ -1,10 +1,10 @@
-import IntroductionButton from '@/components/IntroductionButton';
 import { Empty } from 'antd';
 import isMobileJudge from 'is-mobile';
 import { useMemo } from 'react';
 import { history, useLocation } from 'umi';
+import IntroductionButton from '@/components/IntroductionButton';
 import FetchLoginWrapper from '../FetchLoginWrapper';
-import Layout from '../Listlayout';
+import Layout from '../ListLayout';
 
 const GlobalLayout = (props: any) => {
   const { Component, ...nextProps } = props;
